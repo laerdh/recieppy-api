c7a490b5bc1ceccf1fea326fbbf38f9fd522f0bd
Lars Dahl <laerdh@gmail.com>
2019-01-16 20:38:35 +0100

    Change migration

--- src/main/resources/db/migration/V8__Rename_user_table_name.sql
+++ src/main/resources/db/migration/V8__Rename_user_table_name.sql
@@ -1,1 +1,5 @@
 ALTER TABLE "user" RENAME TO user_account;
+ALTER TABLE user_recipe_list DROP CONSTRAINT user_recipe_list_user_id_fkey;
+ALTER TABLE user_recipe_list
+  ADD CONSTRAINT user_account_recipe_list_user_id_fkey
+    FOREIGN KEY (user_id) REFERENCES user_account(id) ON DELETE CASCADE ON UPDATE CASCADE;
