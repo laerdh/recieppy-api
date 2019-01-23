c76dc6ccf18ac22545b9db59fce0cbd12413c016
Lars Dahl <laerdh@gmail.com>
2019-01-23 22:08:24 +0100

    NotAuthenticated/Authorized extends AuthenticationException

--- src/main/kotlin/com/ledahl/apps/recieppyapi/exception/NotAuthorizedException.kt
+++ src/main/kotlin/com/ledahl/apps/recieppyapi/exception/NotAuthorizedException.kt
@@ -1,5 +1,5 @@
 package com.ledahl.apps.recieppyapi.exception
 
-import java.lang.RuntimeException
+import org.springframework.security.core.AuthenticationException
 
-class NotAuthorizedException(override val message: String = "User not authorized for this operation"): RuntimeException(message)
+class NotAuthorizedException(override val message: String = "User not authorized for this operation"): AuthenticationException(message)
