ba0a702bbd34cca917da37cc80865fc7b221d22c
Lars Dahl <laerdh@gmail.com>
2020-10-13 21:53:50 +0200

    Update build configs

--- src/main/kotlin/com/ledahl/apps/recieppyapi/model/LocationMember.kt
+++
@@ -1,6 +0,0 @@
-package com.ledahl.apps.recieppyapi.model
-
-data class LocationMember(val firstName: String,
-                          val lastName: String,
-                          val email: String? = null,
-                          val isOwner: Boolean = false)
