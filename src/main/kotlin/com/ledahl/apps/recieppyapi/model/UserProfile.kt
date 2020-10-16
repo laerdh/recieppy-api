c1f9d1ecbab1e6e9792d411991a375fb9068063d
Lars Dahl <laerdh@users.noreply.github.com>
2020-10-16 19:59:29 +0200

    Add id to UserProfile (#39)

--- src/main/kotlin/com/ledahl/apps/recieppyapi/model/UserProfile.kt
+++ src/main/kotlin/com/ledahl/apps/recieppyapi/model/UserProfile.kt
@@ -1,5 +1,6 @@
 package com.ledahl.apps.recieppyapi.model
 
-data class UserProfile(val firstName: String,
+data class UserProfile(val id: Long,
+                       val firstName: String,
                        val lastName: String,
                        val email: String? = null)
